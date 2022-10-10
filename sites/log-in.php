21c8c0a94af0136ee1d7cecf4a99a3363f5943dd
Đặng Đức Chính <112791576+ducchinh123@users.noreply.github.com>
2022-10-10 23:40:45 +0700

    chinhdd-tao-xong-form-sign-in

---
+++ sites/log-in.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?=CONTENT_URL?>Images/P-SHOP.png" type="image/x-icon">
+    <title>Trang đăng kí</title>
+    <link rel="stylesheet" href="<?=CONTENT_URL?>css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+   
+
+        <div class="main">
+                <p>Đăng kí</p>
+                <div class="border-bottom"></div>
+
+                <div class="contain_form">
+                        <section class="register_form">
+                                <form action="">
+                                        <h3>Form đăng kí</h3>
+                                        <hr>
+                                        <div class="form_group">
+                                            <input class="input_form" type="text" placeholder=" Điền họ và tên của bạn">
+                                        </div>
+
+                                        <div class="form_group">
+                                            <input class="input_form" type="text" placeholder=" Điền email của bạn">
+                                        </div>
+
+                                        <div class="form_group">
+                                            <input class="input_form" type="text" placeholder=" Điền SĐT của bạn">
+                                        </div>
+
+                                        <div class="form_group">
+                                            <input class="input_form" type="text" placeholder=" Đặt mật khẩu của bạn">
+                                        </div>
+
+
+                            
+                                       <center><button class="submit_dk" type="submit">Đăng kí</button></center> <br>
+                                           <center>hoặc <a href="<?=SITE_URL?>?log-in" style="text-decoration: none;">Đăng nhập</a></center> 
+                                </form>
+                        </section>
+                </div>
+        </div>
+
+        
+          
+    </div>
+</body>
+</html>
