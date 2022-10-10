ffaf2998fcff98f34f796ae13e9a7e1589f1a511
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-10-10 21:56:39 +0700

    luongtv-update-infocart

--- sites/info-cart.php
+++ sites/info-cart.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giỏ hàng</title>
-    <link rel="stylesheet" href="../contents/css/style.css">
+    <link rel="stylesheet" href="<?=CONTENT_URL?>css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 </head>
