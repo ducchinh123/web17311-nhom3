bcd995585ba1c5566d6b1d9fa9102f049cafdf81
Đặng Đức Chính <112791576+ducchinh123@users.noreply.github.com>
2022-10-10 10:15:16 +0700

    chính tạo trang tìm kiếm

--- sites/index.php
+++ sites/index.php
@@ -7,7 +7,14 @@
        $VIEW_NAME="prod.php";
     }elseif(isset($_GET['cart'])){
         $VIEW_NAME="cart.php";
-    }else{
+    }elseif(isset($_GET['search-cate'])){
+        $VIEW_NAME="search-cate.php";
+    }elseif(isset($_GET['search-cate-for-boy'])){
+        $VIEW_NAME="search-cate-for-boy.php";
+    }elseif(isset($_GET['search-cate-for-kid'])){
+        $VIEW_NAME="search-cate-for-kid.php";
+    }
+    else{
         $VIEW_NAME = "pay.php";
 
     }
---
+++ sites/search-cate-for-boy.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Images/P-SHOP.png" type="image/x-icon">
+    <title>Nước hoa nam</title>
+    <link rel="stylesheet" href="<?=CONTENT_URL?>css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+
+            <div class="main">
+                <p><a href="">Sản phẩm</a> > <a href="">Nước hoa nam</a></p>
+                <div class="border-bottom"></div>
+
+
+                <div class="contai_cate">
+                        <div class="cate_title">
+                                <p>Nước hoa nam  <span class="span1">▶</span></p> 
+                        </div>
+
+                        <div class="product_in_cate">
+                                <div class="prod1">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a11.jpg" alt="">
+
+                                   <a href=""><p>LANCOME La vie Est </p></a> 
+
+                                    <p class="price">2.450.000₫</p>
+
+                                </div>
+
+                                <div class="prod2">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a6.jpg" alt="">
+
+                                   <a href=""><p>CHRISTIAN DIOR Miss</p></a> 
+
+                                    <p class="price">2.690.000₫</p>
+                                </div>
+
+
+                                <div class="prod3">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a9.jpg" alt="">
+
+                                    <a href=""><p>Bvlgari Rose Goldea</p></a>
+
+                                    <p class="price">490.000 ₫</p>
+                                </div>
+
+
+                                <div class="prod4">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a3.jpg" alt="">
+
+                                    <a href=""><p>Versace Pour Homme </p></a>
+
+                                    <p class="price">2.068.000 ₫</p>
+                                </div>
+
+
+                                <!-- <span>▶</span> -->
+
+
+                                <div class="prod5">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a1.jpg" alt="">
+
+                                    <a href=""><p>Versace Bright Absolu</p></a>
+
+                                    <p class="price">2.393.000₫</p>
+
+                                </div>
+
+                                <div class="prod6">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a2.jpg" alt="">
+
+                                   <a href=""> <p>Versace Bright Crystal</p></a>
+
+                                    <p class="price">1.701.000₫</p>
+                                </div>
+
+
+                                <div class="prod7">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a5.jpg" alt="">
+
+                                    <a href=""><p>Gucci Bloom For Women</p></a>
+
+                                    <p class="price">2.069.000 ₫</p>
+                                </div>
+
+
+                                <div class="prod8">
+                                    <img src="<?=CONTENT_URL?>Images/sản phẩm/a8.jpg" alt="">
+
+                                    <a href=""><p>Lolita Lempicka For Women</p></a>
+
+                                    <p class="price">1.600.000₫</p>
+                                </div>
+                                
+
+                                
+                                
+                        </div>
+
+
+
+                </div>
+            </div>
+
+
+          
+
+               
+          
+    </div>
+</body>
+</html>
