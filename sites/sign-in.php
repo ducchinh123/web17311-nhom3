012e90ed9fc397a25da99b670991b053720fb1db
doanvanmanh03 <115381407+doanvanmanh03@users.noreply.github.com>
2022-10-11 15:35:00 +0700

    manh-dang-nhap

---
+++ sites/sign-in.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập</title>
+    <link rel="stylesheet" href="<?=CONTENT_URL?>css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="main">
+
+            <div class="main-top">
+                <p>Đăng nhập</p>
+            </div>
+
+            <div class="back">
+                <div class="change-password">
+                    <div class="top">
+                        <p>Đăng nhập</p>
+                    </div>
+
+                    <div class="form">
+                        <input type="text" name="" id="" placeholder="Tên đăng nhập"> <br>
+                        <input type="password" name="" id="" placeholder="Mật khẩu"> <br>
+                        
+                    </div>
+
+                    <div class="bottom">
+                       
+                    <button type="submit">Chấp nhận</button>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       
+    </div>
+</body>
+
+</html>
