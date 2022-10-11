b1d9e880fb82a75d89e52a07eb024dac5f07769b
Đặng Đức Chính <112791576+ducchinh123@users.noreply.github.com>
2022-10-11 16:07:55 +0700

    chinhdd-tao-DAO-prod-img

---
+++ dao/product-img.php
@@ -0,0 +1,45 @@
+<?php
+
+   // Truy vấn dữ liệu
+
+   function select_imgAll(){
+        $sql= "select * from product_img";
+
+        return pdo_query($sql);
+   }
+
+
+   // Thêm mới vào bảng product_Img
+
+   function insert_prod_img($url, $main, $product){
+        $sql= "insert into product_img(url, main, product_id) values(?,?,?)";
+        pdo_execute($sql, $url, $main, $product);
+   }
+
+   // Xóa bản ghi trong product_img
+
+   function destroy_prod_img($id){
+        $sql = "delete from product_img where id=?";
+        pdo_execute($sql,$id);
+   }
+
+
+   // Lấy thông tin một mã product_img
+
+ function get_info($id){
+    $sql = "SELECT * FROM `product_img` where id=?";
+    return pdo_query_one($sql,$id);
+ }
+
+
+   // Sửa dữ liệu trong table product_img
+
+   function update_prod_img($url, $main, $product, $id){
+        $sql = "update product_img set url=?, main=?, product_id=? where id=?";
+        pdo_execute($sql, $url, $main, $product, $id);
+
+
+
+   }
+
+?>
