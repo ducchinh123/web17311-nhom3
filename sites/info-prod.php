8955d0463f6691b48e5ee36c26566b4699a53985
doanvanmanh03 <115381407+doanvanmanh03@users.noreply.github.com>
2022-10-11 14:49:21 +0700

    manhtaothongtinsanpham

---
+++ sites/info-prod.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông tin sản phẩm</title>
+    <link rel="stylesheet" href="dangnhap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    
+        
+        <p class="text-[#FFB6C1] mt-3 ml-3">
+            Trang chủ > Nước hoa nữ > CHRISTIAN DIOR Miss
+        </p>
+        <hr>
+        <section class="pro-info w-[812px] mx-auto mt-10">
+            <div class="info flex ">
+                <div class="image w-[226px] h-[180px] border border-[#000000] rounded-[40px]">
+                    <img class="w-[133px] h-[141px] mx-auto mt-5" src="Images/image 21.png" alt="">
+                    <h4 class="font-[600] border-b-2 border-[#000000] mt-5 ">
+                        Sản Phẩm Liên Quan
+                    </h4>
+                    <div class="sub-image mt-5 flex space-x-[20px]">
+                        <img class="w-[64px] h-[48px] border border-[#000000] p-[10px]" src="Images/Danh mục/d1.jpg" alt="">
+                        <img class="w-[64px] h-[48px] border border-[#000000] p-[10px]" src="Images/Danh mục/d2.jpg" alt="">
+                        <img class="w-[64px] h-[48px] border border-[#000000] p-[10px]" src="Images/Danh mục/d3.jpg" alt="">
+                    </div>
+                </div>
+                <div class="detail-info ml-[60px] leading-8">
+                    <p>Tên: <span class="text-[16px] font-[400]">CHRISTIAN DIOR Miss</span></p>
+                    <p>Thương hiệu: <span >Dior</span></p>
+                    <p>Size: S M XL</p>
+                    <p>Số lượng: 
+                        <button>-</button>
+                        <span>1</span>
+                        <button>+</button>
+                    </p>
+
+                    <button class="mt-6 px-4 bg-[#FFB6C1] font-[400] text-[16px] rounded-[5px]">
+                        Thêm vào giỏ
+                    </button>
+                </div>
+                <div class="price mt-[120px] text-left h-[25px] px-5 font-[400] text-[16px] bg-[#FFB6C1] ">
+                    <p class="">Giá: <span class="text-[#FF0000]">2.690.000đ</span></p>
+                </div>
+            </div> <!-- End .info-->
+        </section> <!-- End .pro-info-->
+        <section class="detail w-[812px] mx-auto mt-[90px]">
+            <h4 class="font-[600] w-[136px] border-b-2 border-[#000000]">
+                Thông tin chi tiết
+            </h4>
+            <div class="mt-6 bg-[#FFB6C1] p-4 text-[#FFFFFF]">
+                <p class="text-[#FFFFFF]">
+                    Trong năm 2016, các cô nàng là tín đồ của dòng nước hoa huyền thoại Miss Dior 
+                    chắc hẳn sẽ vô cùng phấn khởi với phiên bản mới nhất của dòng nước hoa này, 
+                    đó là Miss Dior Absolutely Blooming. Với những nốt hương tươi mới của các loại hoa quả,
+                     Miss Dior Absolutely Blooming như là một món quà dành riêng để cổ vũ tinh thần lạc quan, 
+                    tươi vui cho những cô gái đang đắm chìm trong tình yêu cuộc sống.
+                </p>
+                <div class=" mt-4 mb-4">
+                    <img class="w-[162px] h-[88px] mx-auto" src="Images/image 21.png" alt="">
+                </div>
+                <p >
+                    Miss Dior trước đó nhưng Miss Dior Absolute Blooming có vẻ như thể hiện một khía cạnh mới: 
+                    gợi cảm, đầy đam mê và hiện đại hơn.
+                </p>
+            </div>
+        </section> <!-- End .detail-->
+
+        <section class="comment w-[812px] mx-auto mt-[20px]">
+            <h4 class="font-[600] w-[80px] border-b-2 border-[#000000]">
+                Bình luận
+            </h4>
+            <form action="">
+                <div class="mt-3 flex">
+                    <input class="bg-[#D9D9D9] w-[760px] py-[10px]"
+                        type="text">
+                    <input class="bg-[#D9D9D9] w-[50px] py-[10px] border-[#000000] border-l-[3px]"
+                        type="submit">
+                </div>
+                
+            </form>
+        </section>
+       
+   
+    
+</body>
+
+</html>
