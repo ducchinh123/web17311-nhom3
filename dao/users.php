256867cce27230136f651608c467b35510193984
Đặng Đức Chính <112791576+ducchinh123@users.noreply.github.com>
2022-10-19 10:57:29 +0700

    chinhdd-xong-dao-user

---
+++ dao/users.php
@@ -0,0 +1,39 @@
+<?php
+
+    // Truy vấn dữ liệu
+
+    function select_all_user(){
+        $sql = "select * from users";
+        return pdo_query($sql);
+    }
+
+    // Thêm dữ dữ liệu
+
+    function inser_in_user($name_user, $password, $confirm, $email, $sdt, $role){
+        $sql = "insert into users(name_user, password, confirm, email, sdt, role_id) values(?, ?, ?, ?, ?, ?)";
+        pdo_execute($sql, $name_user, $password, $confirm, $email, $sdt, $role);
+    }
+
+    // xóa
+
+    function delete_user($id){
+        $sql = "delete from users where id=?";
+        pdo_execute($sql, $id);
+    }
+
+    // lấy dữ liệu theo mã
+
+    function get_one_user($id){
+        $sql = "select * from users where id=?";
+        return pdo_query_one($sql, $id);
+    }
+
+    // Cập nhật
+
+    function update_user($name, $email, $sdt, $role, $id){
+            $sql = "update users set name_user=?, email=?, sdt=?, role_id=? where id=?";
+            pdo_execute($sql, $name, $email, $sdt, $role, $id);
+    }
+
+
+?>
