1365cb6ed8822e2c6a0bf386dd080f017fdbb337
doanvanmanh03 <115381407+doanvanmanh03@users.noreply.github.com>
2022-10-23 01:21:07 +0700

    truong-admin-users

--- admin/admin-user/list-user.php
+++ admin/admin-user/list-user.php
@@ -1,6 +1,32 @@
 <?php
     require_once '../dao/pdo.php';
+    require_once '../dao/roles.php';
     require_once '../dao/users.php';
+
+    $dsROLE=get_All();
+    // if(isset($_POST['inser_in_user'])){
+    //     inser_in_user($_POST['name'],$_POST['name_user'],$_POST['password'],$_POST['email'],$_POST['sdt']);
+    //   }elseif(isset($_GET['delete_user']))
+    //   {
+    //     deleteUser($_GET['delete_user']);
+    //   }elseif(isset($_POST['update_user']))
+    //   {
+    //     updateUser($_POST['name'],$_POST['name_user'],$_POST['password'],$_POST['email'],$_POST['sdt'],$_POST['id']);
+    //   }
+
+
+
+
+    if (isset($_POST['inser_in_user'])) {
+        inser_in_user($_POST['name_user'], $_POST['password'], $_POST['confirm'], $_POST['email'], $_POST['sdt'], $_POST['role']);
+        header('location: http://localhost/web17311-nhom3/admin/?list-user');
+    }
+
+
+
+    // hiển thị tất cả các user
+
+    $dsUSER= select_all_user();
     
 ?>
 <!DOCTYPE html>
@@ -27,34 +53,45 @@
                 
                 <h3>Vai trò</h3>
                
-                <select class="input_cate" name="" id="">
-                        <option value="">A</option>
-                        <option value="">B</option>
-                        <option value="">C</option>
+                <select class="input_cate" name="role" id="">
+                        <?php
+
+                        foreach ($dsROLE as $key) {
+                            
+                       
+
+                ?>
+                        <option value="<?php echo $key['id']  ?>"><?php echo $key['name']  ?></option>
+
+
+                        <?php
+                         }
+
+                        ?>
                 </select>
 
               
                 <h3>Tên người dùng</h3> 
-                <input type="text" class="input_form" name="" id=""> <br>
+                <input type="text" class="input_form" name="name_user" id=""> <br>
 
                 <h3>Mật khẩu/Password</h3> 
-                <input type="text" class="input_img" name="" id=""> <br>
+                <input type="password" class="input_img" name="password" id=""> <br>
 
                 <h3>Nhập lại mật khẩu/Confirm Password</h3> 
-                <input type="text" class="input_form" name="" id=""> <br>
+                <input type="password" class="input_form" name="confirm" id=""> <br>
 
 
                 <h3>Địa chỉ Email</h3> 
-                <input type="email" class="input_form" name="" id=""> <br>
+                <input type="email" class="input_form" name="email" id=""> <br>
 
                 
                 <h3>Số điện thoại</h3> 
-                <input type="number" class="input_form" name="" id=""> <br>
+                <input type="number" class="input_form" name="sdt" id=""> <br>
 
 
                 
                 
-                <input type="submit" class="input_form_2" value="Thêm vào">
+                <input type="submit" class="input_form_2" name="inser_in_user" value="Thêm vào">
     
             </form>
             
@@ -71,20 +108,39 @@
                     <th>Email</th>
                     <th>SĐT</th>
                     <th>Vai trò</th>
-                   
                     <th>Quản lí</th>
                 </tr>
+
+
+
                 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><a href="" style="text-decoration: none;">Xóa</a> | <a href="" style="text-decoration: none;">Sửa</a></td>
-                    
+         
+
+
+                <?php
+
+                        foreach ($dsUSER as $user) {
+                          
+                      
+                ?>
+
+<tr>
+                    <td><?php echo $user['id']  ?></td>
+                    <td><?php echo $user['name_user']  ?></td>
+                    <td><?php echo $user['password']  ?></td>
+                    <td><?php echo $user['confirm']  ?></td>
+                    <td><?php echo $user['email']  ?></td>
+                    <td><?php echo $user['sdt']  ?></td>
+                    <td><?php echo $user['role_id']  ?></td>
+                    <td><a href="" style="text-decoration: none;">Xóa</a> 
+                    | <a href="" style="text-decoration: none;">Sửa</a></td>      
+</tr>
+                  <?php
+
+                        }
+            ?>
+
+              
             </table>
         </div>
     </div>
