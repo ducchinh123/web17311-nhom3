3623d3816891ef95f4a3d57e8845690352b88c04
Đặng Đức Chính <112791576+ducchinh123@users.noreply.github.com>
2022-10-23 03:48:44 +0700

    chinh-insert-file

---
+++ global.php
@@ -0,0 +1,9 @@
+<?php
+
+    const BASE_URL = "http://localhost/web17311-nhom3/";
+    const SITE_URL = BASE_URL."sites/index.php";
+    const CONTENT_URL = BASE_URL."contents/";
+    const DAO_URL = BASE_URL."dao/";
+    const ADMIN_URL = BASE_URL."admin/";
+
+?>
